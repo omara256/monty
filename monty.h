e6a61af91e7f1185f2408d83685909621aab5c1a
omara256 <omaraperker56@gamil.com>
2022-06-16 00:12:03 -0700

    Refactored code

---
+++ monty.h
@@ -0,0 +1,85 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * struct globals_s - struct contains global variables.
+ * @data: value to initialize nodes.
+ * @fp: a file pointer.
+ * @lineptr: pointer to a dynamic buffer.
+ * @token: a pointer to the next token.
+*/
+typedef struct globals_s
+{
+	int data;
+	FILE *fp;
+	char *lineptr, *token;
+} globals_t;
+
+extern globals_t globals;
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/* opfunct1 */
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void free_stack(stack_t *stack);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+
+/* opfunct2 */
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void divide(stack_t **stack, unsigned int line_number);
+
+/* opfunct3 */
+void mul(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
+
+/* opfunct4 */
+void rotr(stack_t **stack, unsigned int line_number);
+void push_q(stack_t **stack, unsigned int line_number);
+
+/* monty_check */
+void error_handle(stack_t **stack, unsigned int line_number, int error_type);
+void check_op(stack_t **stack, unsigned int line_number);
+void parse_arg(int argc, char *argv[]);
+void read_line(stack_t **stack);
+void parse_num(stack_t **stack, unsigned int line_number);
+
+#endif /* MONTY_H */
